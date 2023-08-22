11ae69514d0aa1a54648f179edc076aa3243eb25
Charles Leighton <chleighton1@gmail.com>
2023-08-22 13:22:02 -0400

    Initial commit for rps-ui

--- index.js
+++ index.js
@@ -27,34 +27,103 @@ function playRound(playerSelection, computerSelection, score) {
     return alert("Tie game. Try again");
   } else if (results[playerSelection] === computerSelection) {
     score["player"]++;
-    return alert(
-      `You Win! ${playerSelection} beats ${computerSelection}\nScore - Player: ${score["player"]} Computer: ${score["computer"]}`
-    );
+    return alert(`You Win! ${playerSelection} beats ${computerSelection}`);
   } else {
     score["computer"]++;
-    return alert(
-      `You Lose! ${computerSelection} beats ${playerSelection}\nScore - Player: ${score["player"]} Computer: ${score["computer"]}`
-    );
+    return alert(`You Lose! ${computerSelection} beats ${playerSelection}`);
   }
 }
+
+function updateScore(playerScore, computerScore) {
+  playerScore.textContent = `Player: ${score["player"]}`;
+  computerScore.textContent = `Computer: ${score["computer"]}`;
+}
+
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
 
 let score = {
   player: 0,
   computer: 0,
 };
-for (let x = 0; x < 5; x++) {
+
+let playerScore = document.getElementById("player-score");
+let computerScore = document.getElementById("computer-score");
+
+updateScore(playerScore, computerScore);
+
+rock.addEventListener("click", () => {
   let computer = getComputerChoice();
   console.log(computer);
 
-  let player = getPlayerSelection();
+  let player = "Rock";
   console.log(player);
 
   playRound(player, computer, score);
+  updateScore(playerScore, computerScore);
   if (score["player"] === 3) {
     alert("Round over, you win!");
-    break;
+    score = {
+      player: 0,
+      computer: 0,
+    };
   } else if (score["computer"] === 3) {
     alert("Round over, computer wins!");
-    break;
+    score = {
+      player: 0,
+      computer: 0,
+    };
   }
-}
+  updateScore(playerScore, computerScore);
+});
+
+paper.addEventListener("click", () => {
+  let computer = getComputerChoice();
+  console.log(computer);
+
+  let player = "Paper";
+  console.log(player);
+
+  playRound(player, computer, score);
+  updateScore(playerScore, computerScore);
+  if (score["player"] === 3) {
+    alert("Round over, you win!");
+    score = {
+      player: 0,
+      computer: 0,
+    };
+  } else if (score["computer"] === 3) {
+    alert("Round over, computer wins!");
+    score = {
+      player: 0,
+      computer: 0,
+    };
+  }
+  updateScore(playerScore, computerScore);
+});
+
+scissors.addEventListener("click", () => {
+  let computer = getComputerChoice();
+  console.log(computer);
+
+  let player = "Scissors";
+  console.log(player);
+
+  playRound(player, computer, score);
+  updateScore(playerScore, computerScore);
+  if (score["player"] === 3) {
+    alert("Round over, you win!");
+    score = {
+      player: 0,
+      computer: 0,
+    };
+  } else if (score["computer"] === 3) {
+    alert("Round over, computer wins!");
+    score = {
+      player: 0,
+      computer: 0,
+    };
+  }
+  updateScore(playerScore, computerScore);
+});
